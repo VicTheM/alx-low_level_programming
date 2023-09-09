35d5e309ce1ab468f997bc695d96fdfb6bf52219
VicTheM <victorychibuike121@gmail.com>
2023-09-09 23:18:11 +0100

    leme

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,9 @@
 
 /*
  * main - Entry point
- *
+ * description: prints out sizes of data types
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
