4b16c0f9c1d406342ac757ca37914e717bd89da3
VicTheM <victorychibuike121@gmail.com>
2023-09-11 23:50:49 +0100

    done with fifrst file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - entry point
+  *
+  * Return: always 0 for success
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d ", n);
+	if (n < 0)
+		printf("is negative\n");
+	else if (n > 0)
+		printf("is positive\n");
+	else
+		printf("is zero\n");
+	return (0);
+}
