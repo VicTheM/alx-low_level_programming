c1c46c53095f65793e86570dee31d29ee0953fb4
VicTheM <victorychibuike121@gmail.com>
2023-09-13 23:22:02 +0100

    hopefully this works

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,4 +6,23 @@
  * return: always 0 (success)
  */
 int main(void)
+{
+	double first = 1;
+	double second = 2;
+	double nth;
+	int counter;
+
+	printf("%lf, %lf, ", first, second);
+
+	for (counter = 2; counter < 98; counter++)
+	{
+		nth = first + second;
+		printf("%lf, ", nth);
+		first = second;
+		second = nth;
+	}
+	printf("\n");
+
+	return (0);
+}
 
