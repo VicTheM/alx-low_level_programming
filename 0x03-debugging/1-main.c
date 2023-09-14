480576371d2dc239786f8389aa414a7b7b21efdf
VicTheM <victorychibuike121@gmail.com>
2023-09-14 23:34:18 +0100

    commented out the infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,10 +12,10 @@ int main(void)
 
         i = 0;
 
-        /*while (i < 1)*/
+        /*while (i < 1)
 	{
 		putchar(i);
-        }
+        }*/
 
         printf("Infinite loop avoided! \\o/\n");
 
