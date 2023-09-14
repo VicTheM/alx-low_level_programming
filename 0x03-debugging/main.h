706d8da3518f344a7b67d53a8b57670c5c50432b
VicTheM <victorychibuike121@gmail.com>
2023-09-15 00:16:39 +0100

    modified the leap year checker rule

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@ iifndef MAIN_H
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int day, int month);
 void print_remaining_days(int a, int b, int c);
 
 #endif /* MAIN_H */
