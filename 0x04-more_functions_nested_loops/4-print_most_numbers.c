d496d5a63143b84f6f9f1698cc7103d59d448804
VicTheM <victorychibuike121@gmail.com>
2023-09-16 19:53:48 +0100

    fixed bug in 4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,11 @@
  * print_most_numbers - checks if a character is upper
  * Description: prints using a UDF
  *
- * return: Always 1 (true) 0 (false)
+ * Return: Always 1 (true) 0 (false)
  */
 void print_most_numbers(void)
 {
-	int *char_pointer;
-
-	*char_pointer = "01356789\n";
+	char *char_pointer = "01356789\n";
 	
 	while (*char_pointer)
 	{
