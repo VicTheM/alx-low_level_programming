f015d3ce1e43e4c2720b9626c8bead1628797a8b
VicTheM <victorychibuike121@gmail.com>
2023-09-21 20:51:05 +0100

    modified 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,11 +18,9 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[ii] = src[ii];
 	}
-	dest[ii] = '\0';
-	while (ii < n)
+	for ( ; ii < n; ii++)
 	{
 		dest[ii] = '\0';
-		ii++;
 	}
 
 	return (dest);
