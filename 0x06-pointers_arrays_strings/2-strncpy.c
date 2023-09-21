c0d5f3d87d7a283213ed1f882ff9dee5b1564443
VicTheM <victorychibuike121@gmail.com>
2023-09-21 20:47:06 +0100

    first commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies the content of one string into another
+ * @dest: destination buffer
+ * @src: source string
+ * @n: num of byte to be copied
+ *
+ * Return: @dest
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int ii;
+
+	for (ii = 0; ii < n && src[ii] != '\0'; ii++)
+	{
+		dest[ii] = src[ii];
+	}
+	dest[ii] = '\0';
+	while (ii < n)
+	{
+		dest[ii] = '\0';
+		ii++;
+	}
+
+	return (dest);
+}
