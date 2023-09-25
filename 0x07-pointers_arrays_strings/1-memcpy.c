bd5495174d3d0464fb157369573d8e4093411f0a
VicTheM <victorychibuike121@gmail.com>
2023-09-25 16:20:04 +0100

    first commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * memcpy - copiees one memory into another
+ * @dest: destination memory
+ * @src: source memory
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to dest;
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int ii;
+
+	for (ii = 0; ii < n; ii++)
+		dest[ii] = src[ii];
+
+	return (0);
+}
