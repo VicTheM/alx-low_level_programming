c9d3941287bc4c38804a5031c78b6fc17a8e493f
VicTheM <victorychibuike121@gmail.com>
2023-09-25 16:48:23 +0100

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a char in a string buffer
+ * @s: pointer to string buffer
+ * @c: char to search for
+ *
+ * Return: pointer to first occurence of c
+ */
+char *_strchr(char *s, char c)
+{
+	char *cc;
+	int ii = 0;
+
+	while (s[ii])
+	{
+		if (s[ii] == c)
+		{
+			cc = &s[ii];
+			return (cc);
+		}
+		ii++;
+	}
+
+	return ('\0');
+}
