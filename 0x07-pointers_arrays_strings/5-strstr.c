e253200f35ac45f7dc45e32756ac7d9b9911a9d0
VicTheM <victorychibuike121@gmail.com>
2023-09-25 21:15:50 +0100

    first commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - finds a string in a string
+ * @haystack: larger string
+ * @needle: search string
+ *
+ * Return: pointer to begining of found string
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *hay = haystack;
+	char *sch = needle;
+	int ii;
+	int k;
+	int len = 0;
+
+	/* get size of ned */
+	while (hay[len] != '\0')
+		len++;
+	for (ii = 0; ii < len; ii++)
+	{
+		k = 0;
+		while (sch[0] = hay[ii])
+		{
+			k++;
+			if (sch[k] == '\0')
+				return (&hay[ii]);
+			if (sch[k] != hay[ii + k])
+				break;
+		}
+	}
+	return ('\0');
+}
