a6865e4e32c1db3fa8fc37f1911c3306739a1ee1
VicTheM <victorychibuike121@gmail.com>
2023-09-29 01:40:17 +0100

    added helper function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,31 @@
 #include "main.h"
 /**
+ * sqrt_helper - expands its mother function
+ * @n: subject
+ * @iterator: incrementable divisor
+ *
+ * Return: sqrt of n
+ */
+int sqrt_helper(int n, int iterator)
+{
+	if (iterator > n / 2)
+		return (-1);
+	if (iterator * iterator == n)
+		return (iterator);
+	return (sqrt_helper(n, iterator + 1));
+}
+
+/**
  * _sqrt_recursion - returns natural sqrt of int
  * @n: integer
  *
  * Return: square root as int
  */
 int _sqrt_recursion(int n)
+{
+	if (n < 1)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (sqrt_helper(n, 2));
+}
