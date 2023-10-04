50f73cae3446c6344f8b365d3e7e986fc49d71d7
VicTheM <victorychibuike121@gmail.com>
2023-10-04 11:46:13 +0100

    second commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int counter;
 	char *array;
 
-	array = malloc(sizeof(char) * (size + 1));
+	array = malloc(sizeof(char) * (size));
 	if (array == NULL || size == 0)
 	{
 		return (NULL);
