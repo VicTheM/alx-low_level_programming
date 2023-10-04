1ca1ddc42e372eab3dbfdb2b4d310587a6258d6f
VicTheM <victorychibuike121@gmail.com>
2023-10-04 11:37:12 +0100

    first commit'

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int counter;
+	char *array;
+
+	array = malloc(sizeof(char) * (size + 1));
+	if (array == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (counter = 0; counter < size; counter++)
+		{
+			array[counter] = c;
+		}
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+# The files in this folder demonstrates the use of 
+# memory allocation functions like malloc() and free()
+
+all files whose names consist only of digits are eexecutables
+generation by testing the .c files
+
+to be updated later
