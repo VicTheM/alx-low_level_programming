a600b6188c8f0630500a6210f4a3dfc23576ea08
VicTheM <victorychibuike121@gmail.com>
2023-10-05 23:53:38 +0100

    first commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,2 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * alloc_grid - creates 2-D array and initialize to 0
+ * @width: numbere of columns
+ * @height: number of rows
+ *
+ * Return: double pointer to this array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = (int **)malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -28,7 +28,7 @@ int main(void)
  * @size: the size of the memory to print
  *
  * Return: Nothing.
- */
+ 
 void simple_print_buffer(char *buffer, unsigned int size)
 {
     unsigned int i;
@@ -50,7 +50,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
     printf("\n");
 }
 
-/**
+
  * main - check the code for ALX School students.
  *
  * Return: Always 0.
@@ -69,7 +69,7 @@ int main(void)
     free(buffer);
     return (0);
 }
-*/
+
 
 
 int main(void)
@@ -84,5 +84,46 @@ int main(void)
     }
     printf("%s\n", s);
     free(s);
+    return (0);
+}*/
+
+void print_grid(int **grid, int width, int height)
+{
+    int w;
+    int h;
+
+    h = 0;
+    while (h < height)
+    {
+        w = 0;
+        while (w < width)
+        {
+            printf("%d ", grid[h][w]);
+            w++;
+        }
+        printf("\n");
+        h++;
+    }   
+}
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int **grid;
+
+    grid = alloc_grid(6, 4);
+    if (grid == NULL)
+    {
+        return (1);
+    }
+    print_grid(grid, 6, 4);
+    printf("\n");
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+    print_grid(grid, 6, 4);
     return (0);
 }
