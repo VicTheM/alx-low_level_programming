77eed0ffbed47a26e5bcab47424c5630514830df
VicTheM <victorychibuike121@gmail.com>
2023-10-06 12:40:49 +0100

    first commit

---
+++ 0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints out the name of the executable
+ * @argc: numbwr of command line arguments
+ * @argv: array of pointers to strings 
+ *
+ * Return: 0 Success
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
