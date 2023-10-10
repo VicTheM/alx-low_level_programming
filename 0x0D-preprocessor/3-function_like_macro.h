f77dd73073e4f4da95a705635caed0301c7fed40
VicTheM <victorychibuike121@gmail.com>
2023-10-10 21:11:33 +0100

    final

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? ((x) = -(x)) : (x))
+#define ABS(X) ((X) < 0 ? -(X) : (X))
 
 #endif /* _FUNCTION_LIKE_MACRO_H */
