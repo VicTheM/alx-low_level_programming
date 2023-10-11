32906e23c2ad27260f9c86681cf4080673da0e4f
VicTheM <victorychibuike121@gmail.com>
2023-10-12 00:43:46 +0100

    first commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int op1, op2, result;
+	char *operator;
+	int (*funcc)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	op1 = atoi(argv[1]);
+	op2 = atoi(argv[3]);
+	operator = argv[2];
+
+	funcc = get_op_func(operator);
+	if (funcc == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = funcc(op1, op2);
+	printf("%d\n", result);
+
+	return (0);
+}
