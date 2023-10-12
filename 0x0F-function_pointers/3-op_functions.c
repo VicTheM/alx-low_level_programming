fd7c0bacb2c699c929b779c41b0fbebb1f9bfae7
VicTheM <victorychibuike121@gmail.com>
2023-10-12 01:10:07 +0100

    almost donne

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,25 +1,58 @@
 #include "3-calc.h"
-
+/**
+ * op_add - add
+ * @a: a
+ * @b: b
+ *
+ * Return: int
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - sub
+ * @a: a
+ * @b: b
+ *
+ * Return: int
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - mul
+ * @a: a
+ * @b: b
+ *
+ * Return: int
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+ * op_div - div
+ * @a: a
+ * @b: b
+ *
+ * Return: int
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+/**
+ * op_mod - mod
+ * @a: a
+ * @b: b
+ *
+ * Return: int
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
