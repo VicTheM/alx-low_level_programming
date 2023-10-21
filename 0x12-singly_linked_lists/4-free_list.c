434774e49ad6503f227beeb94695046a2a3dcb50
VicTheM <victorychibuike121@gmail.com>
2023-10-21 08:42:20 +0100

    unstable state

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head node
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while(head)
+	{
+		next = head;
+		head = head->next;
+		free(next->str);
+		free(next);
+	}
+	free(head);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# Singly Linked List
+# file 5 was written in assemblly!
+# it feels so cool to write in assemly.
