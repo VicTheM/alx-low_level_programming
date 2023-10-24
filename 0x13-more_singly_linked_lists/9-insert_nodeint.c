fdb0d6a0364f2084cdfad48dabb632734a069d1f
VicTheM <victorychibuike121@gmail.com>
2023-10-25 00:11:48 +0100

    fixed bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *head;
-		*head = new;
+	/*	*head = new; */
 		return (new);
 	}
 	prev = temp = *head;
