fa7e3324d963928457ed77eb9a68e5683328e6b1
root <root@VICTORY-PC>
2023-11-06 19:56:40 +0100

    stable

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,4 +6,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void file_from_error(char *s, char *buf);
+void file_to_error(char *s, char *buf);
+void close_fd(int fd);
 #endif /* _MAIN_H_ */
